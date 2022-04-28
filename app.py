1adcafab43d9434c7a9a7a5292f1893860ca3fa5
kellynichols99 <kellynichols99@gmail.com>
2022-04-27 20:20:24 -0500

    Module 9 lesson 4

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route('/')
+def about_me():
+    return 'I am a data analyst'
